1c66f03bc0ad2850ff2dd1098ec7ce628305ef09
joeHollander <fr3nchon1onsoup@gmail.com>
2024-07-30 17:11:41 -0400

    some

--- Equities/IntradayModel.py
+++ Equities/IntradayModel.py
@@ -1,24 +1,22 @@
 from nautilus_trader.core.data import Data
 from nautilus_trader.common.actor import Actor, ActorConfig
 from nautilus_trader.core.datetime import dt_to_unix_nanos, unix_nanos_to_dt, format_iso8601
-from nautilus_trader.core.data import Data
 from nautilus_trader.model.data import DataType
 from nautilus_trader.serialization.base import register_serializable_type
 from nautilus_trader.model.identifiers import InstrumentId
-import msgspecs
+import msgspec
 
 
 def unix_nanos_to_str(unix_nanos):
     return format_iso8601(unix_nanos_to_dt(unix_nanos))
 
 class BoundsData(Data):
-    def __init__(self, instrument_id: str, upper_bound_data: float, lower_bound_data: float,
-                  ts_event=0, ts_init=0):
-        super().__init__(ts_init=ts_init, ts_event=ts_init)
+    def __init__(self, instrument_id: str, upper_bound_data: float, lower_bound_data: float, ts_event=0, ts_init=0):
+        super().__init__()
 
         self.instrument_id = instrument_id
-        self.ts_event = ts_event
-        self.ts_init = ts_init
+        self.ts_event = 0
+        self.ts_init = 0
 
         self.upper_bound_data = upper_bound_data
         self.lower_bound_data = lower_bound_data
@@ -32,10 +30,18 @@ class BoundsData(Data):
     @property
     def ts_event(self):
         return self._ts_event
+    
+    @ts_event.setter
+    def ts_event(self, value):
+        self._ts_event = value
 
     @property
     def ts_init(self):
         return self._ts_init
+    
+    @ts_init.setter
+    def ts_init(self, value):
+        self.ts_init = value
 
     def to_dict(self):
         return {
