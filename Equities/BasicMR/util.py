92603e8838347b544595a27ee1cce26641af1728
joeHollander <fr3nchon1onsoup@gmail.com>
2024-08-24 13:59:14 -0400

    working!

--- Equities/BasicMR/util.py
+++ Equities/BasicMR/util.py
@@ -4,10 +4,15 @@ import numpy as np
 import yfinance as yf
 import pandas_market_calendars as mcal
 from datetime import datetime
+from datetime import time 
 import matplotlib.pyplot as plt
+from typing import Literal
 
 # function to convert yfinance bar data to timeseries
-def yf_to_timeseries(df, periods_per_day=7):
+def yf_to_timeseries(df, periods_per_day=7, exchange: Literal["NYSE", "NASDAQ"] = "NYSE"):
+    # adjusting weird yf format 
+    df.index = pd.DatetimeIndex(df.index.strftime("%Y-%m-%d %H:%M"))
+
     # find start and end
     start = pd.to_datetime(df.index[0])
     end = pd.to_datetime(df.index[-1])
@@ -16,11 +21,9 @@ def yf_to_timeseries(df, periods_per_day=7):
     holidays = mcal.get_calendar('NYSE').holidays()
     holidays = list(holidays.holidays) # NYSE Holidays
 
-    # get business days
-    day_diff = np.busday_count(start.date(), end.date(), holidays=holidays)
+    # get business days (end date isn't counted so adding another)
+    day_diff = np.busday_count(start.date(), end.date() + pd.Timedelta(days=1), holidays=holidays)
     
-    #days_allowed = day_diff - (day_diff % 5)
-
     # crate array to hold open close values
     oc = np.zeros((len(df) + day_diff, ))
 
@@ -29,16 +32,16 @@ def yf_to_timeseries(df, periods_per_day=7):
     # close values
     oc_idx = np.ones((len(oc), ), dtype=bool)
     oc_idx[::8] = False
-    oc[oc_idx] = df.loc[:, "Adj Close"].values[:sum(oc_idx)]
+    oc[oc_idx] = df.loc[:, "Adj Close"].values
 
     # creating index for dates
     idx = np.ones((len(oc), ), dtype=bool)
     idx[7::8] = False
-    dates = np.zeros(oc.shape, dtype="datetime64[ns]")
+    dates = np.zeros(oc.shape, dtype=datetime)
     dates[idx] = df.index
     # setting index for end of dates
-    later_index = df.index + pd.Timedelta(minutes=30)
-    dates[~idx] = later_index[6::7].values[:sum(~idx)]
+    eod_index = [x.date() for x in df.index]
+    dates[~idx] = [datetime.combine(date, time(hour=16)) for date in eod_index[6::7]]
 
     # create and return new df
     new_df = pd.DataFrame(data=oc, index=dates, columns=["Price"])
@@ -47,5 +50,4 @@ def yf_to_timeseries(df, periods_per_day=7):
 if __name__ == "__main__":
     aapl = yf.download("AAPL", "2024-01-01", "2024-01-31", interval="1h")
     aapl = yf_to_timeseries(aapl)
-    aapl.plot()
-    plt.show()
+    print(aapl.head(10))
