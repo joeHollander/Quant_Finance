370fc3d0262b23887ba6a895be3bac24fbf0f294
joeHollander <fr3nchon1onsoup@gmail.com>
2024-09-07 21:32:27 -0400

    steaming orderbook with ccxt

--- Crypto/StreamingOrderBook.py
+++ Crypto/StreamingOrderBook.py
@@ -1,60 +1,27 @@
-
+import ccxt.pro
 import asyncio
-from websockets import connect
-import aiofiles
 import sys
-import json
-import httpx
 import time
-from datetime import datetime
-from pathlib import Path
+
+if sys.platform == 'win32':
+	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+async def main():
+    exchange = ccxt.pro.kraken({
+        'options': {
+            'defaultType': 'future',
+        },
+    })
+    symbol = 'BTC/USDT'
+    time_now = time.time()
+    while time.time() - time_now < 1:
+        try:
+            orderbook = await exchange.watch_order_book(symbol, limit=10)
+            #print("time: ", round(time.time() * 1000),"bids: ", sorted(orderbook['bids'][:3], key=lambda x: x[0]), "asks: ", sorted(orderbook['asks'][:3], key=lambda x: x[0]))\
+            print(orderbook)
+        except Exception as e:
+            print(type(e).__name__, str(e))
+    await exchange.close()
 
 
-
-async def orderbook_download(pair):
-
-    pair_lower = pair.lower()
-    websocket_url = f"wss://stream.binance.com:9443/ws/{pair_lower}@depth"
-    rest_url = f"https://api.binance.com/api/v3/depth"
-
-    params = {
-            "symbol":pair.upper(),
-            "limit":5000,
-            }
-    today = datetime.now().date()
-
-    async with httpx.AsyncClient() as client:
-        snapshot = await client.get(rest_url, params=params)
-
-    snapshot = snapshot.json()
-    snapshot["time"] = time.time()
-
-    snapshot_file = Path(f"/{pair_lower}-snapshots-{today}.txt")
-    if snapshot_file.is_file():
-        smode = "w"
-    else:
-        smode = "a"
-
-    async with aiofiles.open(f"{pair_lower}-snapshots-{today}.txt", mode = smode) as f:
-        await f.write(json.dumps(snapshot) + "\n")
-
-    updates_file = Path(f"/{pair_lower}-updates-{today}.txt")
-    if updates_file.is_file():
-        umode = "w"
-    else:
-        umode = "a"
-
-    async with connect(websocket_url) as websocket:
-
-        while True:
-            data = await websocket.recv()
-            print(data)
-
-            async with aiofiles.open(f"{pair_lower}-updates-{today}.txt", mode = "a") as f:
-                await f.write(data + "\n")
-
-    print("Hello")
-
-    pass
-
-asyncio.run(orderbook_download("BTCUSDT"))
+asyncio.run(main())
