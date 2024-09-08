cc4d8e7e17525defdecf3d841aeedcac04a09d05
joeHollander <fr3nchon1onsoup@gmail.com>
2024-09-08 12:03:00 -0400

    time latency working and plotting

--- Crypto/DisplayingOrderBook.py
+++ Crypto/DisplayingOrderBook.py
@@ -11,23 +11,30 @@ res = k.get_order_book("ENS/USD", 20)
 offers = pd.DataFrame(res[0], dtype=float)
 bids = pd.DataFrame(res[1], dtype=float)
 
-# if not os.path.isfile("Data/orderbook.txt"):
-#     f = open("Data/orderbook.txt", "x")
-# else:
-#     f = open("Data/orderbook.txt", "a")
+class OBDisplay():
+    def __init__(self, offers: pd.DataFrame, bids: pd.DataFrame):
+        bids.sort_values(by="price", ascending=True, inplace=True)
+        offers.sort_values(by="price", ascending=True, inplace=True)
+
+        # cumulative volume
+        bids["total"] = (bids["volume"][::-1].cumsum())[::-1]
+        offers["total"] = offers["volume"].cumsum()
+
+        self.offers = offers
+        self.bids = bids
+
+    def plot_total(self):
+        
+
+
 
 # plotting
-bids.sort_values(by="price", ascending=False, inplace=True)
-offers.sort_values(by="price", ascending=True, inplace=True)
+bar_width = 0.25  # reduce width to leave space between bars
+bids_offset = bids["price"].values - bar_width / 2  # shift left
+offers_offset = offers["price"].values + bar_width / 2  # shift right
 
-bids["total"] = bids["volume"].cumsum()
-offers["total"] = offers["volume"].cumsum()
-
-
-plt.bar(bids["price"], bids["total"])
-
-plt.bar(bids["price"], bids["total"], width=0.5, color='green', label='Bids')
-plt.bar(offers["price"], offers["total"], width=0.5, color='red', label='Offers')
+plt.bar(bids_offset, bids["total"].values, width=bar_width, color='green', label='Bids')
+plt.bar(offers_offset, offers["total"].values, width=bar_width, color='red', label='Offers')
 
 plt.xlabel("Price")
 plt.ylabel("Cumulative Volume")
@@ -37,4 +44,5 @@ plt.show()
 
 # vwap
 vwap = np.average(np.append(bids["price"].values, offers["price"].values), weights=np.append(bids["volume"].values, offers["volume"].values))
-print(vwap)
+#print(vwap)
+print(bids)
