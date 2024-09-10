539e4885ce5cb18ab316062469d19c0eb45938ff
Joe Hollander <fr3nchon1onsoup@gmail.com>
2024-09-09 22:26:21 -0400

    fixed vvwap and vwap

--- Crypto/DisplayingOrderBook.py
+++ Crypto/DisplayingOrderBook.py
@@ -6,17 +6,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class OBDisplay():
-    def __init__(self, orderbook=None, bar_width=0.25):
+    def __init__(self, orderbook=None, bids=None, offers=None, bar_width=0.25):
         plt.ion()
         self.fig, self.ax = plt.subplots()
         self.legend_added = False
         self.bar_width = bar_width
         self.bid_bars, self.offer_bars = None, None
+
         
-        if not orderbook:
-            return
+        if orderbook is not None:
+            self.bids, self.offers = self.process_orderbook(orderbook)
+        elif bids is not None and offers is not None:
+            self.bids, self.offers = bids, offers
         
-        self.bids, self.offers = self.process_orderbook(orderbook)
+        
+        self.best_bid = self.bids.iloc[0]
+        self.best_offer = self.offers.iloc[0]
+        self.mid = (self.best_bid + self.best_offer) / 2
 
         self.bids_offset = self.bids["price"].values - bar_width / 2  # shift left
         self.offers_offset = self.offers["price"].values + bar_width / 2  # shift right
@@ -71,9 +77,19 @@ class OBDisplay():
         plt.pause(0.001)
         self.fig.canvas.flush_events()
 
-    def vwap(self, exponent=1):
+    def vwap(self, orderbook=None, exponent=1, mid=False):
+        if orderbook is not None:
+            self.bids, self.offers = self.process_orderbook(orderbook)
+            
+        self.best_bid = self.bids["price"].iloc[0]
+        self.best_offer = self.offers["price"].iloc[0]
+        self.mid = (self.best_bid + self.best_offer) / 2
+
         vwap = np.average(np.append(self.bids["price"].values, self.offers["price"].values), weights=np.append(self.bids["volume"].values, self.offers["volume"].values)**exponent)
-        return vwap
+        res = 2 * self.mid - vwap
+        if mid:
+            return self.mid
+        return res
 
     def vvwap(self, v_exponent=1, var_exponent=1):
         self.bids["diff"] = 0.01 -(self.bids["price"] - self.bids["price"].iloc[-1])
@@ -85,15 +101,16 @@ class OBDisplay():
 if __name__ == "__main__":
     api = krakenex.API()
     k = KrakenAPI(api)
-    res = k.get_order_book("ENS/USD", 20)
-    offers = pd.DataFrame(res[0], dtype=float)
-    bids = pd.DataFrame(res[1], dtype=float)
-
-    obd = OBDisplay(offers, bids)
+    bids, offers = k.get_order_book("ENS/USD", 20)
+    
+    bids = bids.astype(np.float64)
+    offers = offers.astype(np.float64)
+    obd = OBDisplay(bids=bids, offers=offers)
     print("normal vwap: ", obd.vwap())
     print("squared vwap: ", obd.vwap(exponent=2))
     print("normal var vwap: ", obd.vvwap())
     print("squared var vwap: ", obd.vvwap(v_exponent=1, var_exponent=2))
-    obd.plot_total()
+    print(obd.mid)
+    #obd.plot_total()
 
 
